88110f69ef376bbf5b3fb5404c644fc39bfb4331
Jonte88 <jonathan.thj15@gmail.com>
2023-12-11 11:38:04 +0100

    IDK 	modified:   Matkort/src/App.jsx

--- Matkort/src/App.jsx
+++ Matkort/src/App.jsx
@@ -3,18 +3,30 @@ import "./App.css";
 
 function App() {
   const [checkboxes, setCheckboxes] = useState([
-    { id: 1, checked: false, name: "Old Times", address: "Bruksgatan 24" },
-    { id: 2, checked: false, name: "Något annat", address: "Bruksgatan 24" },
+    {
+      id: 1,
+      checked: false,
+      name: "Old Times",
+      address: "Bruksgatan 24",
+      category: "Hamburgare",
+    },
+    {
+      id: 2,
+      checked: false,
+      name: "Något annat",
+      address: "Bruksgatan 24",
+      category: "Kaninmat",
+    },
     {
       id: 3,
       checked: false,
       name: "Något helt annat",
       address: "En annan gata 24",
+      category: "Kaninmat",
     },
     // Add more checkbox objects if needed
   ]);
 
-  // Function to handle selecting all checkboxes
   const selectAllCheckboxes = () => {
     const updatedCheckboxes = checkboxes.map((checkbox) => ({
       ...checkbox,
@@ -23,7 +35,6 @@ function App() {
     setCheckboxes(updatedCheckboxes);
   };
 
-  // Function to handle deselecting all checkboxes
   const deselectAllCheckboxes = () => {
     const updatedCheckboxes = checkboxes.map((checkbox) => ({
       ...checkbox,
@@ -32,17 +43,19 @@ function App() {
     setCheckboxes(updatedCheckboxes);
   };
 
-  // Function to toggle individual checkbox
   const checkboxChange = (id) => {
-    console.log("Aaaaaaaaa");
     const updatedCheckboxes = checkboxes.map((checkbox) =>
       checkbox.id === id
         ? { ...checkbox, checked: !checkbox.checked }
         : checkbox
     );
     setCheckboxes(updatedCheckboxes);
-    console.log(checkboxes);
   };
+
+  // Get unique categories from checkboxes
+  const categories = Array.from(
+    new Set(checkboxes.map((checkbox) => checkbox.category))
+  );
 
   return (
     <>
@@ -51,31 +64,36 @@ function App() {
           <p onClick={selectAllCheckboxes}>Select all</p>
           <p onClick={deselectAllCheckboxes}>Deselect all</p>
         </div>
-        <div className="categoryBox">
-          {checkboxes.map((checkbox) => (
-            <div className="restaurantBox" key={checkbox.id}>
-              <div className="custom-checkbox">
-                <input
-                  type="checkbox"
-                  className="hidden-checkbox"
-                  checked={checkbox.checked}
-                  onChange={() => checkboxChange(checkbox.id)}
-                  id={`checkbox-${checkbox.id}`}
-                />
-                <label
-                  className="checkbox-label"
-                  htmlFor={`checkbox-${checkbox.id}`}
-                >
-                  {/* You can add a label text here if needed */}
-                </label>
-              </div>
-              <div className="resturantBoxInfo">
-                <h3>{checkbox.name}</h3>
-                <p>{checkbox.address}</p>
-              </div>
-              <div className="restaurantBoxMoreInfo">
-                <span>i</span>
-              </div>
+        <div className="categoryContainer">
+          {categories.map((category) => (
+            <div key={category} className="categoryBox">
+              <h3>{category}</h3>
+              {checkboxes
+                .filter((checkbox) => checkbox.category === category)
+                .map((checkbox) => (
+                  <div className="restaurantBox" key={checkbox.id}>
+                    <div className="custom-checkbox">
+                      <input
+                        type="checkbox"
+                        className="hidden-checkbox"
+                        checked={checkbox.checked}
+                        onChange={() => checkboxChange(checkbox.id)}
+                        id={`checkbox-${checkbox.id}`}
+                      />
+                      <label
+                        className="checkbox-label"
+                        htmlFor={`checkbox-${checkbox.id}`}
+                      ></label>
+                    </div>
+                    <div className="resturantBoxInfo">
+                      <h3>{checkbox.name}</h3>
+                      <p>{checkbox.address}</p>
+                    </div>
+                    <div className="restaurantBoxMoreInfo">
+                      <span>i</span>
+                    </div>
+                  </div>
+                ))}
             </div>
           ))}
         </div>
